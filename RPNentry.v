081e2c3533cee0a8521f6656ad85ca4c2f77576b
gabrielEuMesmo <95380319+gabrielEuMesmo@users.noreply.github.com>
2025-10-24 14:51:41 -0300

    Add files via upload

--- RPNentry.v
+++ RPNentry.v
@@ -4,30 +4,36 @@ module RPNentry(E, S0, S1, S2, bt, CK, switch1, switch2);
 	output [6:0]S0, S1, S2;
 	
 	wire [1:0]cont;
-	wire [3:0] BCD0, BCD1, BCD2, DeciCont;
+	wire [3:0] BCD0, BCD1, BCD2, DeciCont,SaidaBase0, SaidaBase1,SaidaBase2;
 	wire btDebounced;
-	wire [7:0]S, B, A;
+	wire [7:0] B, A, Saida;
 	
 	debouncer(bt, CK, btDebounced);
 	counterRPN(btDebounced, cont);
 	binarioPDeci3(cont, DeciCont);
 	
-	operacoes(S, B, Cin, RstOuQ, Op, A, , , , );
+	operacoes(A, B, Cin, RstOuQ, Op, SaidaOp, , , , );
 	
 	CarryIn(switch1, Cin, CK, DeciCont[0]);
 	
 	and(RstOuQ, DeciCont[3], switch1);
 	
-	Reg8bits(S, CK, E, DeciCont[0]);
+	Reg8bits(A, CK, E, DeciCont[0]);
 	Reg8bits(B, CK, E, DeciCont[1]);
 	Reg3bits(Op, CK, E[2:0], DeciCont[2]);
 	Reg3bits(Base, CK, E[2:0], DeciCont[3]);
 	
-	BinaryToBCD(A, BCD0, BCD1, BCD2);
+	Reg8bits(Saida, CK, SaidaOp, DeciCont[3]);
 	
-	decodificador7seg(BCD0, S0);
-	decodificador7seg(BCD1, S1);
-	decodificador7seg(BCD2, S2);
+	BinaryToBCD(Saida, BCD0, BCD1, BCD2);
+	
+	escolherBase(BCD0, Saida[3:0], Saida[2:0], Base, SaidaBase0);
+	escolherBase(BCD1, Saida[7:4], Saida[5:3], Base, SaidaBase1);
+	escolherBase(BCD2, , Saida[7:6], Base, SaidaBase2);
+	
+	decodificador7seg(SaidaBase0, S0);
+	decodificador7seg(SaidaBase1, S1);
+	decodificador7seg(SaidaBase2, S2);
 	
 	
 endmodule
---
+++ escolherBase.v
@@ -0,0 +1,14 @@
+module escolherBase(A, B, C, S, Z);
+
+	input [3:0] A, B, C;
+	
+	input [1:0] S;
+	
+	output [3:0] Z;
+	
+	mux2(A[0], B[0], C[0], S, Z[0]);
+	mux2(A[1], B[1], C[1], S, Z[1]);
+	mux2(A[2], B[2], C[2], S, Z[2]);
+	mux2(A[3], B[3], C[3], S, Z[3]);
+	
+endmodule
